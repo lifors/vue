285b9eb2ee2f929cc802ebe953bfc1fc7a68679b
oleg.zemlyansky <oleg.zemlyansky@constructor.ru>
2023-08-28 03:36:46 +0300

    up

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav>
+    <router-link to="/">Задание 1</router-link>
+    <router-link to="/quest-two">Задание 2</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+nav a:first-child {
+  margin-right: 5em;
+}
+a:hover {
+  color: white;
+  background: #42b983;
+  padding: 10px 20px 10px 20px;
+  border-radius: 30px;
+  transition: 0.5s;
+}
+nav {
+  padding: 30px;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    text-decoration: none;
+    transition: 0.5s;
+    font-family: ALS Sector, serif;
+
+    &.router-link-exact-active {
+      color: white;
+      background: #42b983;
+      padding: 10px 20px 10px 20px;
+      border-radius: 30px;
+      text-decoration: none;
+    }
+  }
+}
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Задание 1",
+    component: HomeView,
+  },
+  {
+    path: "/quest-two",
+    name: "Задание 2",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
